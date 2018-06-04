4b64f90102ec6e2efd578db086dd5de79ba7231a
simondahlberg89@gmail.com <simondahlberg89@gmail.com>
2018-06-04 23:09:14 +0200

    Extra chaos

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let id = setInterval(frame, 1)
+let id = setInterval(frame, 5)
 let pos = 0;
 let pos2 = 500;
+let pos3 = 250;
 let flip = false;
 function frame(){
   if(pos > 200 || pos < 0)
@@ -9,8 +10,11 @@ function frame(){
     let change = 4;
     var entity = document.getElementById("entity");
     var entity2 = document.getElementById("entity2");
+    var entity3 = document.getElementById("entity3");
     pos += flip ? change : -change;
     pos2 += flip ? -change : change;
+    pos3 += flip ? -change : change;
     entity.style.left = pos+'px';
     entity2.style.left = pos2+'px';
+    entity3.style.top = pos3+'px';
 }
